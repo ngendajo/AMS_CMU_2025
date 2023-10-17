748d014cc44d3830d651dfb0d056727cb4bc6612
ngendajo <52460758+ngendajo@users.noreply.github.com>
2023-10-17 17:50:34 +0200

    local

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -67,7 +67,7 @@ SECRET_KEY = 'django-insecure-5ye&$4bd)ss*-p8pp%z7f5nj4@br=2!hp_2i3b(dbb@_om#zok
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost:8000']
 """ ALLOWED_HOSTS = ['10.10.88.228','backend.asyv.ac.rw'] """
 
 
