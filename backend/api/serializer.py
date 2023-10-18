8c9ac0d9f075a06e81805bea78fca0e9e5297a39
ngendajo <52460758+ngendajo@users.noreply.github.com>
2023-10-18 07:47:36 +0200

    update alumni list

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -203,12 +203,10 @@ class AlumniListsSerializer(serializers.ModelSerializer):
     family_id = serializers.IntegerField(required=True)
     grade_id=serializers.IntegerField(required=True)
     combination_id = serializers.IntegerField(required=True)
-    decision = serializers.CharField(max_length=30, required=True)
-    life_status = serializers.CharField(max_length=30, required=True)
 
     class Meta:
         model = User
-        fields = ('id','email','image_url','first_name','last_name','phone1', 'grade_name','grade_id','family_name','family_id','combination_name','combination_id','decision','life_status')
+        fields = ('id','email','image_url','first_name','last_name','phone1', 'grade_name','grade_id','family_name','family_id','combination_name','combination_id')
         
 class AlumniListbyEPSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(required=True)
