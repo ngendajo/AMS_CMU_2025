d70a66da5fe04b4624014334cefb17428e696085
ngendajo <52460758+ngendajo@users.noreply.github.com>
2023-10-19 17:38:36 +0200

    Update EditAlumini.jsx

--- frontend/src/components/DashboardComponents/Alumnipart/EditAlumini.jsx
+++ frontend/src/components/DashboardComponents/Alumnipart/EditAlumini.jsx
@@ -84,12 +84,12 @@ export default function EditAlumini() {
             var imgname=email+current+"."+files[0].name.split('.').pop();
             console.log(imgname+":"+files[0].name.split('.').pop())
             console.log(URL.createObjectURL(files[0]))
-            const file = new File(files[0], imgname);
+            const file = new File(files, imgname);
                 
             setDisplayifile(false)
             try{
                 let formData = new FormData();
-                formData.append('image_url',file);
+                formData.append('image_url',files[0]);
                 const response = axios.post(baseUrl+"/updateuserimage/"+params.id,
                     formData,{
                         headers: {
