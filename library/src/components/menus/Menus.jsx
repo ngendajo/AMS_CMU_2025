ee475fff01484e1402d6098fe3cbfad2033f3588
ngendajo <52460758+ngendajo@users.noreply.github.com>
2024-05-09 14:01:24 +0200

    changes in many where librarian can see upload button 3

--- library/src/components/menus/Menus.jsx
+++ library/src/components/menus/Menus.jsx
@@ -110,7 +110,6 @@ export default function Menus() {
                                 <li><Link onClick={toggleClass} className='is-active' to={`/costatistics`}> Statistics per Class </Link> </li>
                             </ul>
                         </li>
-                        
                     </>:
                     user.is_teacher || user.is_student ?
                         <>
@@ -123,14 +122,18 @@ export default function Menus() {
                             
                         
                     }
+                    { user.is_superuser ?
+                            <li onClick={toggleSubmenu} className="menu-item sub__menus__arrows" > <Link to="#"> Uploads <FiChevronDown /> </Link>
+                                <ul className={boxClassSubMenu.join(' ')} > 
+                                    <li className="menu-item " ><Link onClick={toggleClass} className='is-active' to={`/upbook`}> Upload Books </Link> </li>
+                                    <li className="menu-item " ><Link onClick={toggleClass} className='is-active' to={`/upstudent`}> Upload Students </Link> </li>
+                                </ul>
+                            </li>:
+                                <>
+                                
+                                </>}
                     
                     <li className="menu-item " ><Link onClick={toggleClass} className='is-active' to={`/pass`}> Password </Link> </li>
-                    <li onClick={toggleSubmenu} className="menu-item sub__menus__arrows" > <Link to="#"> Uploads <FiChevronDown /> </Link>
-                        <ul className={boxClassSubMenu.join(' ')} > 
-                            <li className="menu-item " ><Link onClick={toggleClass} className='is-active' to={`/upbook`}> Upload Books </Link> </li>
-                            <li className="menu-item " ><Link onClick={toggleClass} className='is-active' to={`/upstudent`}> Upload Students </Link> </li>
-                        </ul>
-                    </li>
 
                     </ul>
 
