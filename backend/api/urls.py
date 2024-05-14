c84a3ccca004a35e7a04b6be962b6eb07cbb3b15
ngendajo <52460758+ngendajo@users.noreply.github.com>
2024-05-14 08:17:28 +0200

    pagination 5

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -158,7 +158,7 @@ urlpatterns = [
     
     #Issue_Book paths
     path('issue/', views.Issue_BookRegistrationView.as_view(), name='issue_book_api'),
-    path('issue/', views.filter_issue_books_by_student, name='filter_issue_books_by_student'),
+    path('issuest/', views.filter_issue_books_by_student, name='filter_issue_books_by_student'),
     path('issue/<int:pk>/delete/', views.delete_Issue_Book, name='delete-issue'),
     path('issue/<int:pk>/', views.update_Issue_Book, name='update_issue'),
     path('change-stpassword/', views.ChangeStudentPasswordView.as_view(), name='change-password'),
