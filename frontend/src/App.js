6f65cd4c45aeec12345e6320bbeec57a6ae3254a
zzzannie <117230878+zzzannie@users.noreply.github.com>
2024-06-13 11:26:02 +0200

    A

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import { MdPhone } from 'react-icons/md';
+import { HiOutlineMail } from 'react-icons/hi';
+import { AiOutlineTwitter } from 'react-icons/ai';
+import { FaFacebook, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
+
+export default function HomeTopBar() {
+    const containerStyle = {
+        marginTop: '20px',
+        marginBottom: '20px',
+        paddingLeft: '5%',
+        paddingRight: '5%'
+    };
+
+    const barStyle = {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    };
+
+    const contactIcon = {
+        fontSize: 23,
+        color: '#F0A459'
+    };
+
+    const paragraphStyle = {
+      fontSize: 13,
+      fontFamily: 'Arial',
+      color: '#575757'
+    };
+
+    const mediaIcon = {
+        fontSize: 16,
+        color: '#F49C46',
+        margin: 5,
+        cursor: 'pointer'
+    };
+
+    return (
+        <div style={containerStyle}>
+            <div style={barStyle}>
+
+                <div className="HomeTopBarLeft">
+                    <div className="HomeTopBarLeftContent">
+                        <MdPhone style={contactIcon} />
+                        <p style={paragraphStyle}>+25073691194</p>
+                    </div>
+                    <div className="HomeTopBarLeftContent">
+                        <HiOutlineMail style={contactIcon} />
+                        <p style={paragraphStyle}>julius@asyv.org</p>
+                    </div>
+                </div>
+
+                <div className="HomeTopBarRight">
+                    <a href="https://www.instagram.com/agahozoshalom/" target="_blank" rel="noopener noreferrer">
+                        <FaInstagram style={mediaIcon} />
+                    </a>
+                    <a href="https:/twitter.com/asyv" target="_blank" rel="noopener noreferrer">
+                        <AiOutlineTwitter style={mediaIcon} />
+                    </a>
+                    <a href="https://www.facebook.com/AgahozoShalom/">
+                        <FaFacebook style={mediaIcon} />
+                    </a>
+                    <a href="https://www.linkedin.com/in/julius-kaboyo?originalSubdomain=rw" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedinIn style={mediaIcon} />
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    );
 }
-
-export default App;
