b932d69825694d3e32e9574ca9ef0965a38e8723
ngendajo <52460758+ngendajo@users.noreply.github.com>
2024-08-01 16:20:03 +0200

    update alumni data using excel

--- backend/userprofile/models.py
+++ backend/userprofile/models.py
@@ -187,7 +187,6 @@ class Studie(models.Model):
 class Employmentdraft(models.Model):
     title = models.CharField(max_length=5000)
     alumn = models.ForeignKey(Alumni, on_delete=models.PROTECT, related_name='employ')
-    emp = models.ForeignKey('Employment', on_delete=models.PROTECT, related_name='employment', null=True, blank=True)
     
     EMPLOYMENT_CHOICES = (
         ('F', 'Full-time'),
