276acff4d7a3989a6c25a2de71b07d0cd26e421b
ngendajo <52460758+ngendajo@users.noreply.github.com>
2024-11-07 08:41:48 +0200

    attendance start

--- backend/api/views.py
+++ backend/api/views.py
@@ -5790,7 +5790,7 @@ class StudentListView(generics.ListAPIView):
 #Attendance Report
 class AttendanceReportView(generics.GenericAPIView):
     serializer_class = AttendanceReportSerializer
-    permission_classes = [IsAuthenticated]
+    #permission_classes = [IsAuthenticated]
 
     def get_queryset(self, start_date=None, end_date=None):
         return Absenteeism.objects.filter(
