13e1578eec7913b60e0061dea1f33564e11e7191
ngendajo <52460758+ngendajo@users.noreply.github.com>
2024-11-20 20:28:08 +0200

    attendance start

--- library/src/components/eaps/EapsAtt.jsx
+++ library/src/components/eaps/EapsAtt.jsx
@@ -91,8 +91,8 @@ export default function EapsAtt() {
                                     "student": student_id,
                                     "status": status
                                 }
-
-                        })
+                            }
+                        )
                     }
                 }
             }
