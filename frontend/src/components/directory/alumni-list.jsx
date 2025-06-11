1b055b41367193612a22c2d30cfa3bc42f6d7a1a
Jiatong Li <102882840+Jiatonglii22@users.noreply.github.com>
2025-06-11 10:59:53 +0200

    integrating alumni directory backend to frontend

--- frontend/src/components/directory/alumni-list.jsx
+++ frontend/src/components/directory/alumni-list.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import "./alumni-list.css";
 
-const AlumniList = ({ alumni, onSelect }) => {
+const AlumniList = ({ alumni, onSelect, lastRef }) => {
     return (
         <div className="alumni-list">
-            {alumni.map((alum) => {
+            {alumni.map((alum, index) => {
+                const isLastItem = index === alumni.length - 1;
                 const profilePic = alum.profilePic;
                 return (
-                    <div key={alum.id} className="alumni-item" onClick={() => onSelect(alum)}>
+                    <div
+                        key={alum.id}
+                        className="alumni-item"
+                        onClick={() => onSelect(alum)}
+                        ref={isLastItem ? lastRef : null}
+                    >
                         <img src={profilePic} alt="Profile" className="alumni-pic" />
-                        <div className="alumni-name">{alum.firstName} {alum.lastName}<br/>({alum.gradeName} - {alum.familyName} -{alum.combinationName})</div>
+                        <div className="alumni-name">
+                            {alum.firstName} {alum.lastName}
+                            <br />
+                            ({alum.gradeName} - {alum.familyName} - {alum.combinationName})
+                        </div>
                     </div>
                 );
             })}
